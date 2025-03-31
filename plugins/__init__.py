7c5f7a3ec7116ea81f416b7191055fd0fc27b2b9
Aaru <aryarai1569@protonmail.com>
2025-03-31 20:57:57 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,15 +2,23 @@ from .client import MangaClient, MangaCard, MangaChapter
 
 # Manga Client
 from .weebcentral import WeebCentralClient
-from .manganelo import ManganeloClient
 from .comick import ComickClient
 from .mangadex import MangaDexClient
 from .asurascans import AsuraScansClient
 from .flamecomics import FlameComicsClient
 from .mangabuddy import MangaBuddyClient
+#from .reaperscans import ReaperScansClient
+#from .manhwaclan import ManhwaClanClient
+#from .mangamob import MangaMobClient
 
 # Novel Client
 from .novelbuddy import NovelBuddyClient
 from .novelhall import NovelHallClient
 from .wuxiaworld import WuxiaWorldClient
 from .readnovel import ReadNovelClient
+
+#18+ Client
+#from .mangadistrict import MangaDistrictClient
+#from .manga18fx import Manga18fxClient
+#from .manhwa18cc import Manhwa18ccClient
+#from .omegascans import OmegaScansClient
